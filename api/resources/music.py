98312cfe3a3b7987ee7e00c7c74c24ed05e32a9b
Cole Loewer <colemloewer@gmail.com>
2022-08-03 21:13:29 -0500

    Simplified MusicRequests, moved to abort() over error returns.

--- api/resources/music.py
+++ api/resources/music.py
@@ -1,4 +1,4 @@
-from flask_restful import Resource, reqparse
+from flask_restful import Resource, reqparse, abort
 
 from api.models.music import MusicRequestModel
 from api.lookup.music import MusicLookup
@@ -9,45 +9,47 @@ class MusicRequest(Resource):
     parser.add_argument('title', type=str, help="Music Track Title")
     parser.add_argument('album', type=str, help="Music Album")
     parser.add_argument('artist', type=str, help="Music Artist")
-    parser.add_argument('id', type=int, help="Deezer music id.")
-    parser.add_argument('music_type', type=str, help="Music type [album, track]")
+
+    id_parser = reqparse.RequestParser()
+    id_parser.add_argument('id', type=int, required=True, help="Deezer_id Required")
+    id_parser.add_argument('music_type', type=str, required=True, help="Music type [album, track] Required.")
 
     def get(self) -> (dict, int):
         data = MusicRequest.parser.parse_args()
-        title = data.get("title")
-        album = data.get("album")
-        artist = data.get("artist")
+        title, album, artist = data.get("title"), data.get("album"), data.get("artist")
+
+        if not (title or album or artist):
+            abort(400, message="Requires title, album, or artist to search with.")
 
         if title:
             music = MusicLookup.search_for_track(title, artist, album)
             if not music:
-                return {'message': f"Unable to find track(s) with - Title {title}, Artist: {artist}, Album: {album}"}, 400
+                abort(404, message=f"Track(s) with - Title {title}, Artist: {artist}, Album: {album} not found.")
         elif album:
             music = MusicLookup.search_for_album(album, artist)
             if not music:
-                return {'message': f"Unable to find album(s) with - Title: {album}, Artist: {artist}"}, 400
-        elif artist:
+                abort(404, message=f"Album(s) with - Title: {album}, Artist: {artist} not found.")
+        else:
             music = MusicLookup.search_for_artist(artist)
             if not music:
-                return {'message': f"Unable to find track(s) with - Artist: {artist}"}, 400
-        else:
-            return {'message': f'Requires title, album, or artist, or deezer id to search with.'}, 400
+                abort(404, message=f"Track(s) from - Artist: {artist}")
 
         return music, 200
 
     # @jwt_required()
     def post(self) -> (dict, int):
-        data = MusicRequest.parser.parse_args()
-        deezer_id = data["id"]
-        music_type = data["music_type"]
+        data = MusicRequest.id_parser.parse_args()
+        deezer_id, music_type = data.get("id"), data.get("music_type")
 
-        if deezer_id and music_type:
-            music = MusicLookup.lookup_by_deezer_id(deezer_id, music_type)
-            if not music:
-                return {'message': f"Unable to find {music_type} with - Deezer Id: {deezer_id}"}, 400
-            music = MusicRequestModel(**music)
-        else:
-            return {'message': f'Requires deezer id.'}, 400
+        if MusicRequestModel.find_by_deezer_id(deezer_id):
+            abort(400, message=f"Request for {deezer_id} already exists.")
+
+        music = MusicLookup.lookup_by_deezer_id(deezer_id, music_type)
+
+        if not music:
+            abort(404, message=f"Deezer ID {deezer_id} not found.")
+
+        music = MusicRequestModel(**music)
 
         music.save_to_db()
 
@@ -56,19 +58,16 @@ class MusicRequest(Resource):
     # @jwt_required()
     def delete(self) -> (dict, int):
         # TODO Make sure that user can only delete their own requests, Admins can delete all
-        data = MusicRequest.parser.parse_args()
-
+        data = MusicRequest.id_parser.parse_args()
         deezer_id = data.get("id")
-
-        if not deezer_id:
-            return {'message': f"Requires deezer id for request lookup."}, 400
 
         music = MusicRequestModel.find_by_deezer_id(deezer_id)
 
         if not music:
-            return {'message': f'Request does not exist.'}, 404
+            abort(404, message=f"Request does not exist.")
 
         music.delete_from_db()
+
         return {'message': f'Request deleted successfully.'}, 200
 
 
